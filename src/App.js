95421ca6a82880fa21a15f1541f7ced9124b76ba
Samuel Rider-Leck <samuelriderleck@gmail.com>
2021-01-17 12:17:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Design from './features/design/Design';
 import './App.css';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import Box from '@material-ui/core/Box'
+
 function App() {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box className="App" bgcolor="#24282B">
+        <Design />
+      </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/features/design/Design.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import PanelPicker from './components/PanelPicker'
+
+import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+
+import Checkbox from '@material-ui/core/Checkbox'
+import Collapse from '@material-ui/core/Collapse'
+import MenuItem from '@material-ui/core/MenuItem'
+import Select from '@material-ui/core/Select'
+import Switch from '@material-ui/core/Switch'
+import Typography from '@material-ui/core/Typography'
+
+
+const Design = () => {
+  const panel_array = [
+    "Front",
+    "Top",
+    "Left Side",
+    "Right Side"
+  ]
+
+  const case_array = [
+    "Fractal Design R6",
+    "Case 2",
+    "Case 3"
+  ]
+
+  return <Container fixed>
+    <Grid container justify="space-between" alignItems="flex-end" name="topbar">
+      <Grid item xs>
+        <Box p={2}>
+          <Typography variant="h3">Custom Wrap <br/>Configurator</Typography>
+        </Box>
+      </Grid>
+      <Grid item>
+        <Typography variant="h5">Toggle 3D View:  <Switch name="3dview" /></Typography>
+      </Grid>
+    </Grid>
+    <Grid container>
+      <Grid xs={3} item name="sidebar">
+        <Box name="casepicker" p={1}>
+          <Typography variant="h4">Case:</Typography>
+          <Select name="case" fullWidth  >
+            {
+              case_array.map((item, index)  => {
+                return <MenuItem key={"ca"+index} value={item}>{item}</MenuItem>
+              })
+            }
+          </Select>
+        </Box>
+        <Box>
+          {
+            panel_array.map((string,index) => {
+              return <PanelPicker key={"panelp"+index} title={string}/>
+            })
+          }
+        </Box>
+      </Grid>
+      <Grid item xs name="body">
+        <Box minHeight="50vh" m={5}>
+          Main Image Goes Here
+        </Box>
+        <Typography variant="body2">
+          Preview Blurry?<br/>
+          Hit the upscale button for each individual image to make them look sharper.
+        </Typography>
+      </Grid>
+    </Grid>
+  </Container>
+}
+
+export default Design
